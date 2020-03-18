16037a2105f4ed2f45d8aec33e59d3116a0a8141
Daniel Kersten <dkersten1994@gmail.com>
2020-03-18 14:57:23 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -28,8 +28,10 @@ def get_user_input
   gets
 end
 
-def end_game
+def end_game(number)
   # code #end_game here
+  
+  puts "Sorry, you hit #{number}. Thanks for playing!"
 end
 
 def initial_round
